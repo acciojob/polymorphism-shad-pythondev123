49b9f4d5d77dbeda017415245d2b00ee67f60b6c
shad <princekhan785@gmail.com>
2023-05-16 08:10:35 +0530

    created class product in main fn , created object and applied method overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+public static class Product {
+    public int product(int x, int y) {
+      return x*y;
+    }
 
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+
+
+    public double product(double x, double y) {
+        return x * y;
+    }
+}
+
+public static void main(String[] args){
+    Product p= new Product();
+    int ans= p.product(2,3);
+    int ans2= p.product(2,3,4);
+    double ans3= p.product(5,6);
+//    System.out.println(ans + " "+ ans2 + " " + ans3);
+}
 }
